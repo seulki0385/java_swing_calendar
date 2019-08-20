2e7de706f46042dfc9b4bab5f21c83e913fdf55e
seulki0385 <seulki0385@naver.com>
2019-08-20 20:16:14 +0900

    commit

--- src/study/HelloWorld.java
+++ src/study/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("HelloWorld");
-		System.out.println("HelloJava");
+		System.out.println("HelloJava!");
 
 	}
 
